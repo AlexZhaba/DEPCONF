64b774254854db989867bdfcccbd2357225b3c10
AlexZhaba <programzhava@mail.ru>
2020-09-30 21:37:37 +0300

    what

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+
+import md5 from 'md5';
+
+import {CircularProgress } from '@material-ui/core';
+
+
 let CustomInput = ({title, value, change}) => {
   let [active, setActive] = useState(false);
   return (
     <div className="input__wrapper">
-      <div className='input__title__container' style={active ? {color: "#e3d15b", transition: ".2s all"} : {transition: ".2s all"}}>{title}</div>
+      <div className='input__title__container' style={active ? {color: "#ad5050", transition: ".2s all"} : {transition: ".2s all"}}>{title}</div>
       <input className='reg__input' onFocus={() => setActive(true)} onBlur={() => setActive(false)} value={value} onChange={(event) => change(event.target.value) }
           type={title === "password" || title === "confirm password" ? "password" : "text"}
-          placeholder={''}/>
+          placeholder={""}/>
     </div>
   );
 }
@@ -25,8 +31,27 @@ let SignUp = () => {
   let [email, setEmail] = useState("");
   let [password, setPassword] = useState("");
   let [confPassword, setConfPassword] = useState("")
-  
-  let sendSignInData = () => {
+  let [freeEmailStatus, setFreeEmailStatus] = useState(null);
+  let [isLoading, setIsLoading] = useState(false);
+  // let []
+  // window.emailTime;
+  useEffect(() => {
+    if (email === "") {
+      setFreeEmailStatus(null);
+      clearTimeout(window.emailTime);
+    } else {
+      clearTimeout(window.emailTime);
+      window.emailTime = setTimeout(() => {
+        alert("LOADING")
+      }, 1000)
+    }
+  }, [email])
+  // null - пользователь ничего не вводил или в поле пусто
+  // "LOAD" - получение инфы 
+  // "OK"   - почта свободна 
+  // "ERR"  - почта не свободна
+  let sendSignUpData = () => {
+    setIsLoading(true);
     axios({
       url: "https://60be0604f848.ngrok.io/signup",
       method: "post",
@@ -39,9 +64,10 @@ let SignUp = () => {
         fullName: fullName,
         username: username, 
         email: email, 
-        password: password,
+        password: md5(password),
       })
     }).then(response => {
+      setIsLoading(false);
       alert('nice');
     }).catch(e => {
       
@@ -54,18 +80,51 @@ let SignUp = () => {
       <CustomInput title="email" value={email} change={setEmail}/>
       <CustomInput title="password" value={password} change={setPassword}/>
       <CustomInput title="confirm password" value={confPassword} change={setConfPassword}/>
-      <div className="button__container" onClick={() => sendSignInData()}>
-        AcceptMyFuckingGoodness
+      <div className="shadow__loading" style={!isLoading ? {display: "none"} : {}}>
+        <CircularProgress color='primary'/>
+      </div>
+      <div className="button__container" onClick={() => sendSignUpData()}>
+        Sign up
       </div>
     </div>
   );
 }
 
 let SignIn = () => {
+  let [username, setUsername] = useState("");
+  let [password, setPassword] = useState("");
+  let [isLoading, setIsLoading] = useState(false);
+  let sendSignInData = () => {
+    setIsLoading(true);
+    axios({
+      url: "https://60be0604f848.ngrok.io/signup",
+      method: "post",
+      headers: {
+        // "Access-Control-Allow-Origin" : "https://afc210a26eaa.ngrok.io",
+        // "Access-Control-Allow-Origin" : "*",
+        // "Access-Control-Allow-Credentials": "true",
+      },
+      data: JSON.stringify({
+        username: username,
+        password: md5(password),
+      })
+    }).then(response => {
+      setIsLoading(false);
+      alert('nice');
+    }).catch(e => {
+      
+    })
+  }
   return (
     <div className='signup__container'>
-      <CustomInput title="username"/>
-      <CustomInput title="password"/>
+      <CustomInput title="username" value={username} change={setUsername}/>
+      <CustomInput title="password" value={password} change={setPassword}/>
+      <div className="shadow__loading" style={!isLoading ? {display: "none"} : {}}>
+        <CircularProgress color='primary'/>
+      </div>
+      <div className="button__container" onClick={() => sendSignInData()}>
+        Sign in
+      </div>
     </div>
   )
 }
@@ -77,7 +136,12 @@ let App = () => {
       <div className='main__container'>
           <div className='reg__container'>
             <div className='swap__container'>
-              
+              <div className="swap__element" onClick={() => setActivePage(1)} style={activePage === 1 ? {color: "#e3d15b"} : {}}>
+                SIGN UP
+              </div>
+              <div className="swap__element"onClick={() => setActivePage(2)} style={activePage === 2? {color: "#e3d15b"} : {}}>
+                SIGN IN
+              </div>
             </div>
             <div style={{width: "100%"}}>
               {
